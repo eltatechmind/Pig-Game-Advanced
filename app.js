40813ee78118c62ab96bd6c74b2400b48998f8bd
Elta3lab <ahmedfouad2020era@gmail.com>
2018-08-16 14:50:14 +0200

    creating of the game

---
+++ app.js
@@ -0,0 +1,48 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+
+//creating variables for each player
+var scores, roundScore, activePlayer;
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+// change the styling of element with class .dice and make it dissappear using display: none;
+document.querySelector('.dice').style.display = 'none';
+
+// use get elements by id which is used only to select ids and faster than queryselector
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+// create event listener containing an annonymous function, which can't be called outside the event, check them here: https://developer.mozilla.org/en-US/docs/Web/Events
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    //1. Random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+    
+    //2. Display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+    
+    //3. Update the round score but only IF the rolled number is not one.
+    
+});
+
+
+
+
+
+
+
+
