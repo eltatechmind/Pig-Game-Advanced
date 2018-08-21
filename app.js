ba233bed181561741b5048ce30b5d01dccda72cf
Elta3lab <ahmedfouad2020era@gmail.com>
2018-08-21 03:08:16 +0200

    fix a bug

--- app.js
+++ app.js
@@ -140,7 +140,10 @@ function init(){
     document.getElementById('name-1').textContent = 'Player 2';
     document.querySelector('.player-0-panel').classList.remove('winner');
     document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
     document.querySelector('.player-0-panel').classList.add('active');
+    
 }
 
 
